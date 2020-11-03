ef47dc49ae148c1d4cf0f160f685728e0050f0dc
Leo Janas <leojanas7@gmail.com>
2020-11-03 14:14:37 -0700

    Initial commit with /movie endpoint

---
+++ app.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIEDEX = require('./moviedex.json')
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req,res,next){
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+    console.log(apiToken)
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({error: "Unauthorized request"})
+    }
+
+    next()
+})
+
+function handleGetMovie(req,res){
+    let movies = MOVIEDEX;
+    if(req.query.genre){
+        movies = movies.filter(movie => (
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        ))
+    }
+    if(req.query.country){
+        movies = movies.filter(movie => (
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        ))
+    }
+    if(req.query.avg_vote){
+        if(!parseFloat(req.query.avg_vote)){
+            return res.status(400).send('Invalid query term. avg_vote must be numeric.')
+        }
+        movies = movies.filter(movie => (
+            parseFloat(movie.avg_vote) >= parseFloat(req.query.avg_vote)
+        ))
+    }
+    res.json(movies)
+}
+
+app.get('/movie', handleGetMovie)
+
+module.exports = app;
